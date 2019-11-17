552cd0ca00f8064bcac00b18fe3fa4a5dc60865a
BruceXuheng <BruceXuheng@163.com>
2019-11-17 11:33:09 +0800

    Servlet配置

---
+++ StudyWeb/src/com/csw/jcs/servlet/LoginGenericServlet.java
@@ -0,0 +1,23 @@
+package com.csw.jcs.servlet;
+
+import javax.servlet.GenericServlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet("/loging")
+public class LoginGenericServlet extends GenericServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        System.out.println("ServletParent init ");
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        System.out.println("ServletParent .... ");
+    }
+}
