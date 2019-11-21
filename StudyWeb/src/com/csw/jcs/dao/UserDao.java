80e4f4f0ead76f333c0328263897bb9f37625a07
BruceXuheng <BruceXuheng@163.com>
2019-11-21 22:10:46 +0800

    登录案例实现（2）

--- StudyWeb/src/com/csw/jcs/dao/UserDao.java
+++ StudyWeb/src/com/csw/jcs/dao/UserDao.java
@@ -1,9 +1,15 @@
 package com.csw.jcs.dao;
 
+import com.csw.jcs.bean.UserBean;
+
 public class UserDao {
 
-    public void selectLoginUser(){
+    /**
+     * 登录方法
+     */
 
+    public UserBean login(UserBean loginUser){
+        return loginUser;
     }
 
 }
