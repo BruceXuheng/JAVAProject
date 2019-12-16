2440651c2da0119b9ad9295c6a0bff1b7f04e365
BruceXuheng <BruceXuheng@163.com>
2019-12-16 22:14:20 +0800

    CookiesDemo

--- StudyWeb/src/com/csw/jcs/cookiesedemo/CookieDemo1Servlet.java
+++ StudyWeb/src/com/csw/jcs/cookiesedemo/CookieDemo1Servlet.java
@@ -2,6 +2,7 @@ package com.csw.jcs.cookiesedemo;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +11,10 @@ import java.io.IOException;
 @WebServlet(name = "CookieDemo1Servlet",urlPatterns = "/CookieDemo1Servlet")
 public class CookieDemo1Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        //首次登录 记录Cookie
+        Cookie cookie = new Cookie("lasttime","2018-12-12");
+        //
+        response.addCookie(cookie);
 
     }
 
--- StudyWeb/src/com/csw/jcs/cookiesedemo/CookieDemo2Servlet.java
+++ StudyWeb/src/com/csw/jcs/cookiesedemo/CookieDemo2Servlet.java
@@ -2,18 +2,32 @@ package com.csw.jcs.cookiesedemo;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet(name = "CookieDemo2Servlet",urlPatterns = "/CookieDemo2Servlet")
 public class CookieDemo2Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+        response.setContentType("text/html;charset=utf-8");
+
+        Cookie[] cookies = request.getCookies();
+        if(cookies!=null && cookies.length>0){
+            for (Cookie cookie : cookies) {
+                System.out.println(cookie.getValue()+"--"+cookie.getName());
+                if("lasttime".equals(cookie.getName())){
+                    String lastTImeValue = cookie.getValue();
+                    response.getWriter().write("欢迎回来，您上次访问时间"+lastTImeValue);
+                }
+            }
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        this.doPost(request,response);
     }
 }
