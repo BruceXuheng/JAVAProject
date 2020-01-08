969fb249c15da916543d17e38f99b5a2c5d5d592
BruceXuheng <BruceXuheng@163.com>
2020-01-09 00:09:08 +0800

    油画布局Demo

--- StudyWeb/web/油画商城Demo/js/content.js
+++ StudyWeb/web/油画商城Demo/js/content.js
@@ -1,3 +1,16 @@
-function byId(){
+function byId(id){
+    return typeof(id) === "string" ? document.getElementById(id) : id;
 
+}
+
+function showMenuList(){
+    var menuList = byId("header_item_menu_list");
+    menuList.style.display = "block";
+    console.log("show");
+}
+
+function closeMenuList() {
+    var menuList = byId("header_item_menu_list");
+    menuList.style.display = "none";
+    console.log("closemenulist");
 }
